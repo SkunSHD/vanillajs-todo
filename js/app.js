069b05e529deb31c3a2605a8c0eb13fa817f8cd1
Denis <skunshd@gmail.com>
2016-09-11 18:03:34 +0200

    router -> View

--- js/app.js
+++ js/app.js
@@ -11,9 +11,9 @@
 		this.storage = new app.Store(name);
 		this.model = new app.Model(this.storage);
 		this.template = new app.Template();
-		this.view = new app.View(this.template);
 		this.router = app.Router;
-		this.controller = new app.Controller(this.model, this.view, this.router);
+		this.view = new app.View(this.template, this.router);
+		this.controller = new app.Controller(this.model, this.view);
 	}
 
 	var todo = new Todo('todos-vanillajs');
